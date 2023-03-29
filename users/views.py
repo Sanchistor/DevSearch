1458a3eef9513ef1b50df26fe02b388d0cba9c19
Oleksandr Zadyraka <sashazadiraka@gmail.com>
2023-03-29 18:08:24 +0300

    feat(App): Implement feature of user-profile

--- users/views.py
+++ users/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate, logout
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
@@ -19,26 +19,43 @@ def loginUser(request):
         try:
             user = User.objects.get(username =username)
         except:
-            messages.error(request, "User does not exist")
+            messages.error(request, "User does not exist!")
 
         user = authenticate(request, username = username, password=password)
 
         if user is not None:
             login(request, user)
+            messages.success(request, 'User was successfully logged in!')
             return redirect('profiles')
         else:
-            messages.error(request, 'Username or password is incorrect')
+            messages.error(request, 'Username or password is incorrect!')
 
     return render(request, 'login_register.html')
 
 def logoutUser(request):
     logout(request)
-    messages.error(request, "User was logged out")
+    messages.info(request, "User was logged out!")
     return redirect('login')
 
 def registerUser(request):
     page = 'register'
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('profiles')
+
+        else:
+            messages.error(request, 'An error has occurred during registration')
+
     context = {'page': page, 'form': form}
     return render(request, 'login_register.html', context)
 
@@ -57,3 +74,11 @@ def userProfile(request, pk):
     context = {'profile': profile, 'topSkills': topSkills,
                "otherSkills": otherSkills}
     return render(request, 'user-profile.html', context)
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+    context = {'profile': profile, 'skills': skills, 'projects': projects}
+    return render(request, 'account.html', context)
